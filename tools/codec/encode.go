46052fc647ed2085a32cc3518df7694208117d6b
prataprc <prataprc@gmail.com>
2015-07-31 17:44:20 +0530

    tools/codec: collation overhead computation.

--- tools/codec/encode.go
+++ tools/codec/encode.go
@@ -13,9 +13,10 @@ import "encoding/hex"
 import "github.com/prataprc/collatejson"
 
 var options struct {
-	encode bool
-	decode bool
-	inp    string
+	encode    bool
+	decode    bool
+	overheads bool
+	inp       string
 }
 
 var usageHelp = `Usage : codec [OPTIONS]
@@ -27,6 +28,7 @@ func argParse() {
 	flag.StringVar(&options.inp, "inp", "", "input to encode")
 	flag.BoolVar(&options.encode, "encode", false, "encode input")
 	flag.BoolVar(&options.decode, "decode", false, "decode input")
+	flag.BoolVar(&options.overheads, "overheads", false, "collation overhead")
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, usageHelp)
 		flag.PrintDefaults()
@@ -40,7 +42,10 @@ func main() {
 	argParse()
 	codec := collatejson.NewCodec(100)
 	out := make([]byte, 0, len(options.inp)*3+collatejson.MinBufferSize)
-	if options.encode {
+	if options.overheads {
+		computeOverheads(codec)
+
+	} else if options.encode {
 		out, err = codec.Encode([]byte(options.inp), out)
 		if err != nil {
 			log.Fatal(err)
@@ -64,5 +69,35 @@ func main() {
 		}
 		fmt.Printf("in : %q\n", options.inp)
 		fmt.Printf("out: %q\n", string(out))
+	}
+}
+
+func computeOverheads(codec *collatejson.Codec) {
+	var err error
+
+	items := []string{
+		"10",
+		"10000",
+		"1000000000",
+		"100000000000000000.0",
+		"123456789123565670.0",
+		"10.2",
+		"10.23456789012",
+		"null",
+		"true",
+		"false",
+		`"hello world"`,
+		`[ 10, 10000, 1000000000, 10.2, 10.23456789012, null, true, false, "hello world" ]`,
+		`{ "a": 10000, "b": 10.23456789012, "c": null, "d": true, "e": false, "f": "hello world" }`,
+	}
+	for _, item := range items {
+		out := make([]byte, 0, 1024)
+		out, err = codec.Encode([]byte(item), out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("item: %q\n", item)
+		fmt.Println("  i/p len:", len(item))
+		fmt.Println("  o/p len:", len(out))
 	}
 }
